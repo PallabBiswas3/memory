2621f4a0293647971d7ce232781406d51a38ad3a
PallabBiswas3 <pallabbiswas891@gmail.com>
2024-03-07 18:47:04 +0530

    Made dynamic changed card colour

--- script.js
+++ script.js
@@ -3,7 +3,30 @@ let secondCard = null;
 let canClick = true;
 
 const cards = document.querySelectorAll('.card');
+const colors = ['red', 'green', 'yellow', 'blue', 'brown', 'skyblue', 'violet', 'orange'];
 
+
+function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}
+
+
+function setColors() {
+    const shuffledColors = shuffle(colors.concat(colors));
+    const cards = document.querySelectorAll('.card');
+    cards.forEach((card, index) => {
+        card.classList.remove('hidden');
+        card.classList.remove(...colors);
+        card.classList.add(shuffledColors[index]);
+    });
+    setTimeout(() => {
+        cards.forEach(card => card.classList.add('hidden'));
+    }, 0);
+}
 function flipCard() {
     if (!canClick) return;
     if (this === firstCard) return;
@@ -42,5 +65,5 @@ function resetCards() {
     secondCard = null;
     canClick = true;
 }
-
+window.addEventListener('load', setColors);
 cards.forEach(card => card.addEventListener('click', flipCard));
