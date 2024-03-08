12d9bff64786a01ec2fba7aa628c7772822ae9df
Pallab Biswas <153554497+PallabBiswas3@users.noreply.github.com>
2024-03-08 15:36:23 +0530

    Update script.js
    restarting game after getting maxpoints

--- script.js
+++ script.js
@@ -59,6 +59,7 @@ function checkMatch() {
             document.getElementById('message').innerHTML = '<h3>Congratulations! You have completed the puzzle!</h3>';
             document.getElementById('points').innerHTML = `Points: 16`;
             document.getElementById('startBtnContainer').style.display = 'block';
+            resetGame() ;
             return;
         }
         setTimeout(() => {
